a3e0dde2f3a1bfd90a03fdbe8b2fcf78c70f406f
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-08-27 21:19:42 -0400

    Added another header usually supposed to be provided by the user of libparam, in this case, us, the BCU driver.

---
+++ include/param_config.h
@@ -0,0 +1,17 @@
+/*
+ * param_config.h
+ *
+ *  Created on: Nov 12, 2016
+ *      Author: johan
+ */
+
+#ifndef INCLUDE_PARAM_CONFIG_H_
+#define INCLUDE_PARAM_CONFIG_H_
+
+#define PARAMID_CSP_DEBUG                   50
+
+#define PARAMID_COLLECTOR_CNFSTR            200
+#define PARAMID_COLLECTOR_RUN               201
+#define PARAMID_COLLECTOR_VERBOSE           202
+
+#endif /* INCLUDE_PARAM_CONFIG_H_ */
